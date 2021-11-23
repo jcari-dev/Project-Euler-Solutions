6dee69fa90e809af68a5ce4b377163b9c690eb83
jcari-dev <582119jc@gmail.com>
2021-11-22 23:20:25 -0500

    added daily entry

---
+++ Problem3/code.js
@@ -0,0 +1,20 @@
+function factorsOf(n) {
+    let factors = [];
+    let div = 2;
+  
+    while (n >= 2) {
+      if (n % div == 0) {
+        factors.push(div);
+        n = n / div;
+      } else {
+        div++;
+      }
+    }
+
+
+    return factors[factors.length-1];
+  }
+
+console.log(factorsOf(600851475143))
+
+
