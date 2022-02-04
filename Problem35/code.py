7633572503aa3bb624ced2f79f4ed66b7385c9c4
jcari-dev <582119jc@gmail.com>
2022-02-04 00:18:29 -0500

    fixed problem 35

--- Problem35/code.py
+++ Problem35/code.py
@@ -1,18 +1,24 @@
 import math
 from numpy import roll
+
 d = []
-for num in range(3,1000000,2):
-    if all(num%i!=0 for i in range(2,int(math.sqrt(num))+1)):
-      d.append(num)
-print(d)
+for num in range(3, 1000001, 2):
+    if all(num % i != 0 for i in range(2, int(math.sqrt(num)) + 1)):
+        d.append(num)
+
+count = 0
+
 for val in d:
-  count = 0
-  val_listed = [int(x) for x in str(val)]
-  print(val_listed)
-  for index, integer in enumerate(range(len(val_listed))): 
-    rotation = roll(val_listed, index)
-    rotation = int("".join(map(str, rotation)))
-    # print(rotation)
-    if rotation in d:
-        count +=1
-print(count)
+    val_listed = [int(x) for x in str(val)]
+    full_rotation = len(val_listed)
+    for index, integer in enumerate(range(len(val_listed))):
+        rotation = roll(val_listed, index)
+        rotation = int("".join(map(str, rotation)))
+
+        if rotation in d:
+            full_rotation -= 1
+            if full_rotation == 0:
+                count += 1
+        else:
+            continue
+print(count) #55
