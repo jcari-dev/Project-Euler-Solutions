4cfa9448b05aefdcbf873cc1b71031f12980cc89
jcari-dev <582119jc@gmail.com>
2022-03-01 23:10:25 -0500

    problem12 solution is now right, just slow, needs optimizing

--- Problem12/code.py
+++ Problem12/code.py
@@ -1,24 +1,25 @@
 from functools import reduce
+import math
 
 
-def get_triangle_number(nth):
+def get_triangle_number_more_than(nth):
 
     factors = []
     triangle_number_terms = []
     sum_number_terms = []
     number = 1
 
-    while len(factors) < nth:
+    while not len(factors) > nth:
         triangle_number_terms.append(number)
         terms_sum = reduce(lambda x,y: x+y, triangle_number_terms)
         sum_number_terms.append(terms_sum)
-        factors = [n for n in range(1, terms_sum +1) if terms_sum % n == 0]
+        factors = [n for n in range(1, terms_sum+ 1) if terms_sum % n == 0]
         number += 1
     
         
-    
-    return sum_number_terms, factors, terms_sum
+    number_of_factors = len(factors)
+    return print(f"the number {terms_sum}, has {number_of_factors} factors, which is larger than {nth}")
 
-print(get_triangle_number(100))
+print(get_triangle_number_more_than(500))
     
     
