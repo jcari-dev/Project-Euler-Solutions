1dec50e7c9f6d2cd4d46ab5a22b162bc55e47e7a
Jorge Caridad <582119jc@gmail.com>
2022-12-04 01:00:06 -0500

    forget optimization just simplify your life

--- Problem 92/code.py
+++ Problem 92/code.py
@@ -1,40 +1,25 @@
 
 
-def sumDigits(number, seen=None):
+def sumDigits(number):
     """ Returns the squared sum of the digits of the provided number."""
     chainSum = 0
-    if seen is None:
-        seen_numbers = []
-    else:
-        seen_numbers = seen
 
     for digit in str(number):
         digit = int(digit)
         chainSum = chainSum + digit * digit
 
-    seen_numbers.append(chainSum)
+    if chainSum == 89:
+        return True
+    elif chainSum == 1:
+        return False
 
-    if chainSum in [89, 1]:
-        if chainSum == 89:
-            return seen_numbers
-        elif chainSum == 1:
-            return False
-    return sumDigits(chainSum, seen_numbers)
+    return sumDigits(chainSum)
 
-
-ending_in_89 = []
 
 counter = 0
 
-for number in [85, 89, 145, 42, 20]:
+for number in range(1, 10_000_000):
     if sumDigits(number):
-        if number in ending_in_89:
-            print(number, 'skipped!')
-            counter = counter + 1
-            continue
-        for value in sumDigits(number):
-            if value not in ending_in_89:
-                ending_in_89.append(value)
         counter = counter + 1
 
-print(counter)
+print(counter)  # 8581146
