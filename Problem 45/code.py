bcf660f6f552fe9bf0c69ca4759796c050121a01
Jorge Caridad <582119jc@gmail.com>
2022-12-05 23:27:25 -0500

    Solved

--- Problem 45/code.py
+++ Problem 45/code.py
@@ -2,30 +2,28 @@ from math import sqrt
 
 
 def triangularNumber(number):
+    """ Returns the value of the triangular number entered. """
     return number*(number+1)/2
 
 
-def pentagonalNumber(number):
-    return number*(3 * number-1)/2
+def is_pentagonal(number):
+    """ Returns True if Number is Pentagonal. """
+    result = (sqrt(24*number+1)+1)/6
+    return result.is_integer()
 
 
-def hexagonalNumber(number):
-    return number*(2*number-1)
+def is_hexagonal(number):
+    """ Returns True if Number is Hexagonal. """
+    result = (sqrt(1+8*number)+1)/4
+    return result.is_integer()
 
 
-for t in range(285, 5000):
+t = 285
+flag = False
+while flag is False:
     t_number = triangularNumber(t)
-    for p in range(165, 5000):
-        p_number = pentagonalNumber(p)
-        for h in range(143, 5000):
-            h_number = hexagonalNumber(h)
-            if p_number == h_number and h_number == p_number:
-                if p_number > 40755:
-                    print({
-                        'p_number': p_number,
-                        'p': p,
-                        't': t,
-                        'h': h
-                    })
+    if is_pentagonal(t_number) and is_hexagonal(t_number) and t_number > 40755:
+        flag = True
+    t += 1
 
-print(t_number, p_number, h_number)
+print(t_number)  # 1533776805.0
