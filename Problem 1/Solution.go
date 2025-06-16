a743d8a3dbce8fe1b6a048dee167d6ddec7fb1af
jcari-dev <582119jc@gmail.com>
2025-06-16 13:26:41 -0400

    quick refactor and diving back into go

--- Problem 1/Solution.go
+++ Problem 1/Solution.go
@@ -1,19 +1,14 @@
 package main
 
-import "fmt"
-
-func fizzBuzzSum(upTo int) {
-	sum := 0
-	for i := 0; i < upTo; i++ {
-		if i%3 == 0 || i%5 == 0 {
-			sum += i
-
-		}
-	}
-
-	fmt.Println(sum)
-}
-
 func main() {
-	fizzBuzzSum(1000) // 233168
+	var sum int
+	for i := 1; i < 1000; i++ {
+		if i%3 == 0 {
+			sum += i
+		} else if i%5 == 0 {
+			sum += i
+		}
+
+	}
+	println(sum) // 233168
 }
